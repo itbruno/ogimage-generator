dc395a42034c9fd42f66b567e69afe7c90ebe692
Bruno Rodrigues <brunowebr@gmail.com>
2022-10-23 18:54:54 -0300

    Add and configure puppeteer and aws lambda to generate custom png

---
+++ src/lib/screenshot.ts
@@ -0,0 +1,31 @@
+import chrome from 'chrome-aws-lambda';
+import puppeteer from 'puppeteer-core';
+
+export default async function screenshot(
+  url: string,
+  width: number,
+  height: number
+) {
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === 'win32'
+            ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+            : process.platform === 'linux'
+            ? '/usr/bin/google-chrome'
+            : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      };
+
+  const browser = await puppeteer.launch(options);
+  const page = await browser.newPage();
+
+  await page.setViewport({ width, height });
+  await page.goto(url, { waitUntil: 'networkidle0' });
+  return await page.screenshot({ type: 'png' });
+}
---
+++ src/pages/og-image.tsx
@@ -0,0 +1,40 @@
+import Error from 'next/error';
+import { useRouter } from 'next/router';
+
+function OgImage() {
+  const { query } = useRouter();
+  if (query.title) {
+    return (
+      <>
+        <div
+          style={{
+            fontFamily: 'sans-serif',
+            display: 'grid',
+            gridTemplateColumns: '2fr 1fr',
+            padding: '4rem',
+            backgroundColor: '#333',
+            height: '100vh',
+            color: '#fff'
+          }}
+        >
+          <p>{query.title}</p>
+          <div style={{ position: 'relative', width: 200, height: 300 }}>
+            {query.image && (
+              <img
+                style={{
+                  maxWidth: '100%'
+                }}
+                src={query.image.toString()}
+                alt={query.title.toString()}
+              />
+            )}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return <Error statusCode={404} />;
+  }
+}
+
+export default OgImage;
