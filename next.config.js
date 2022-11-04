eb0f4b2ecb16d322752dcfe5245130ef57b026e4
Bruno Rodrigues <brunowebr@gmail.com>
2022-11-04 00:49:24 -0300

    Add sample image domain to test images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+
+  images: {
+    domains: ['via.placeholder.com']
+  }
 };
 
 module.exports = nextConfig;
