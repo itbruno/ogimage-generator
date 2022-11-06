5df48c6ba431c96770af312ec3179fed2399a1ca
Bruno Rodrigues <brunowebr@gmail.com>
2022-11-06 11:12:53 -0300

    Update image  url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 function Home() {
   const [title, setTitle] = useState('Your title here');
   const [image, setImage] = useState(
-    'https://images.unsplash.com/photo-1667611209964-3e1ff3394a35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+    'https://images.unsplash.com/photo-1667611209964-3e1ff3394a35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80.png'
   );
   const [width, setWidth] = useState(800);
   const [height, setHeight] = useState(600);
